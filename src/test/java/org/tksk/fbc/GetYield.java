e014b3f09fbccb14dc18803425d69b26bceba5ce
tksk <tksk00@gmail.com>
2012-05-29 23:08:33 +0900

    unused import

--- src/test/java/org/tksk/fbc/GetYield.java
+++ src/test/java/org/tksk/fbc/GetYield.java
@@ -5,8 +5,6 @@ import static org.testng.Assert.assertEquals;
 import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.testng.annotations.Test;
 
